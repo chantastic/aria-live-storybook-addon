92ab191c3d2aeea332db2cbe044e934c94ce8952
Michael Chan <mijoch@gmail.com>
2021-07-28 09:52:31 -0700

    Add use-aria-live package for quick examples
    Motivation: Quickly develop example stories for developing the addon
    
    Resources: [use-aria-live](https://www.npmjs.com/package/use-aria-live)
    
    Notes:
    `use-aria-live` is a snipped of code that I've used to control Aria Live
    Regions in other applications. I published it for easy access in today's
    assignment but it is not yet documented.

--- stories/AriaLiveRegions.stories.js
+++ stories/AriaLiveRegions.stories.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useAriaLive, PoliteAriaLive } from "use-aria-live";
 
 function Subject(props) {
-  return <div />;
+  let [politeAnnouncement, announcePolitely] = useAriaLive();
+
+  return (
+    <>
+      <button type="button" onClick={() => announcePolitely("Item added")}>
+        Announce "Item added", politely.
+      </button>
+
+      <div
+        style={{
+          marginTop: "2rem",
+          backgroundColor: "#eee",
+          padding: "1rem",
+          borderRadius: ".25rem",
+        }}
+      >
+        See the current state of the announced value for debugging:
+        <pre>debug: {JSON.stringify(politeAnnouncement)}</pre>
+      </div>
+
+      <PoliteAriaLive>{politeAnnouncement}</PoliteAriaLive>
+    </>
+  );
 }
 
 export default {
@@ -14,8 +37,8 @@ const Template = (args) => <Subject {...args} />;
 export const Simple = Template.bind({});
 Simple.args = {};
 
-export const SmallAppSetup = Template.bind({});
-SmallAppSetup.args = {};
+// export const SmallAppSetup = Template.bind({});
+// SmallAppSetup.args = {};
 
-export const LargeAppSetup = Template.bind({});
-LargeAppSetup.args = {};
+// export const LargeAppSetup = Template.bind({});
+// LargeAppSetup.args = {};
